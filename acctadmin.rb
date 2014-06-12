302f9cccbfcb00b47e29c440f1d7dec192e96bb5
James Liu <jaycliu@gmail.com>
2014-06-12 10:50:50 -0700

    initial commit

---
+++ acctadmin.rb
@@ -0,0 +1,57 @@
+#!/home/jliu/.rbenv/shims/ruby
+
+require 'fox16'
+include Fox
+
+class Acctadmin < FXMainWindow
+  def initialize(app)
+    super(app, 'Acctadmin', width: 200, height: 200)
+    option_menu
+  end
+
+  def create
+    super
+    show(PLACEMENT_SCREEN)
+  end
+
+  def option_menu
+    matrix = FXMatrix.new(self, 3, opts: MATRIX_BY_ROWS|LAYOUT_CENTER_X|LAYOUT_CENTER_Y, vSpacing: 8)
+    createButton = FXButton.new(matrix, "Create User", opts: BUTTON_NORMAL|LAYOUT_FILL_X, padding: 4)
+    searchButton = FXButton.new(matrix, "Search Users", opts: BUTTON_NORMAL|LAYOUT_FILL_X, padding: 4)
+    quitButton = FXButton.new(matrix, "Quit", opts: BUTTON_NORMAL|LAYOUT_FILL_X, padding: 4)
+
+    createButton.connect(SEL_COMMAND, method(:launch_create))
+    searchButton.connect(SEL_COMMAND, method(:launch_search))
+    quitButton.connect(SEL_COMMAND) { exit }
+  end
+
+  def launch_create(sender, sel, ptr)
+    CreateDialog.new(self).execute
+    return 1
+  end
+
+  def launch_search(sender, sel, ptr)
+    SearchDialog.new(self).execute
+    return 1
+  end
+end
+
+class CreateDialog < FXDialogBox
+  def initialize(owner)
+    super(owner, "Create User", DECOR_TITLE|DECOR_BORDER|DECOR_RESIZE)
+  end
+end
+
+class SearchDialog < FXDialogBox
+  def initialize(owner)
+    super(owner, "Search Users", DECOR_TITLE|DECOR_BORDER|DECOR_RESIZE)
+  end
+end
+
+if __FILE__ == $0
+  FXApp.new do |app|
+    Acctadmin.new(app)
+    app.create
+    app.run
+  end
+end
